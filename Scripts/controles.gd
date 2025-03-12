87d6d0bab916f0b61aea55d3f3538b354d1eda31
jp414141 <146342051+jp414141@users.noreply.github.com>
2025-03-12 01:29:56 +0100

    Add files via upload

---
+++ Scripts/controles.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func _on_atras_pressed():
+	get_tree().change_scene_to_file("res://Escenas/menu_principal.tscn")
---
+++ Scripts/escena_final_1.gd
@@ -0,0 +1,4 @@
+extends CanvasLayer
+
+func _on_siguiente_pressed():
+	get_tree().change_scene_to_file("res://Escenas/escena_final_2.tscn")
